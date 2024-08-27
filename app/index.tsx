7aa4b532eb1e283fbdfa373db9de1a2ad6d7af5a
allankruger <allanfloreskruger@gmail.com>
2024-08-27 18:22:28 +0200

    build: add nativewind for styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <Text>VideoBook</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">VideoBook</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Profile
@@ -13,12 +13,3 @@ export default function RootLayout() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
